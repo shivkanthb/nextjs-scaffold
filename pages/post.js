11f1171b2acf838e72045ffd97b911f4dc834400
shivkanthb <shivkanthb@gmail.com>
2017-07-11 23:32:27 -0700

    isometric fetch

--- pages/post.js
+++ pages/post.js
@@ -1,12 +1,25 @@
 import Layout from '../components/Mylayout.js'
+import fetch from 'isomorphic-unfetch'
 
-export default (props) => (
+const Post = (props) => (
     <Layout>
-       <h1>{props.url.query.title}</h1>
-       <p>This is the blog post content.</p>
+       <h1>{props.show.name}</h1>
+       <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+       <img src={props.show.image.medium}/>
     </Layout>
 )
 
+Post.getInitialProps = async function (context) {
+  const { id } = context.query
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+  const show = await res.json()
+
+  console.log(`Fetched show: ${show.name}`)
+
+  return { show }
+}
+
+export default Post
 
 /* 
 The following will not work however 
