5641eb7dc64bb109ed6a12d88d7632042146f0e5
shivkanthb <shivkanthb@gmail.com>
2017-07-18 23:03:16 -0700

    now deploy

--- components/Header.js
+++ components/Header.js
@@ -12,6 +12,9 @@ const Header = () => (
 		<Link href= "/about">
 			<a style={linkStyle}>About</a>
 		</Link>
+		<Link href="/styled_index">
+			<a style={linkStyle}>Styled Home</a>
+		</Link>
 	</div>
 )
 
--- pages/post.js
+++ pages/post.js
@@ -49,3 +49,48 @@ But, if you need, you can pass it like this:
 	)
 
 */
+
+/*
+Global JS Styles example 
+
+
+import Layout from '../components/Mylayout.js'
+import Markdown from 'react-markdown'
+
+export default (props) => (
+  <Layout>
+   <h1>{props.url.query.title}</h1>
+   <div className="markdown">
+     <Markdown source={`
+This is our blog post.
+Yes. We can have a [link](/link).
+And we can have a title as well.
+
+### This is a title
+
+And here's the content.
+     `}/>
+   </div>
+   <style jsx global>{`
+     .markdown {
+       font-family: 'Arial';
+     }
+
+     .markdown a {
+       text-decoration: none;
+       color: blue;
+     }
+
+     .markdown a:hover {
+       opacity: 0.6;
+     }
+
+     .markdown h3 {
+       margin: 0;
+       padding: 0;
+       text-transform: uppercase;
+     }
+  `}</style>
+  </Layout>
+)
+*/
